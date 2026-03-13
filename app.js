34af7048624d2afc36f80e332057375c4b3c6bbb
cashlindsley09 <cashlindsley09@gmail.com>
2026-03-12 22:25:29 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ collection,
 addDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+
 const firebaseConfig = {
-apiKey: "PASTE",
-authDomain: "PASTE",
-projectId: "PASTE",
-storageBucket: "PASTE",
-messagingSenderId: "PASTE",
-appId: "PASTE"
-};
+    apiKey: "AIzaSyCG3BJihg9spqBIx27G7Uadp11af0mGgao",
+    authDomain: "race-mind-research.firebaseapp.com",
+    projectId: "race-mind-research",
+    storageBucket: "race-mind-research.firebasestorage.app",
+    messagingSenderId: "302345130110",
+    appId: "1:302345130110:web:8c21f3ba39da0227e0c82f"
+  };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
 const form = document.getElementById("raceForm");
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", async (e)=>{
 
 e.preventDefault();
 
@@ -42,8 +45,12 @@ performance: document.getElementById("performance").value
 
 await addDoc(collection(db,"runs"),data);
 
-alert("Race submitted!");
+document.getElementById("successMessage").style.display="block";
 
 form.reset();
+
+setTimeout(()=>{
+document.getElementById("successMessage").style.display="none";
+},3000);
 
 });
